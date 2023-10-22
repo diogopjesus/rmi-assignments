eeaba4651739867df17c8953f3dfbf662da48c7f
Diogo Jesus <diogopjesus@ua.pt>
2023-10-22 21:15:37 +0100

    Add script to create submission file.

---
+++ zip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if command -v zip >/dev/null 2>&1 ; then
+    mkdir -p .tmp/agent
+    cp build.sh run.sh mainC1.py mainC2.py mainC3.py requirements.txt .tmp/agent
+    cd .tmp
+    zip -r agent_92338_97596.zip agent
+    mv agent_92338_97596.zip ../
+    cd ..; rm -r .tmp
+else
+    echo "zip not found"
+    return 1
+fi
