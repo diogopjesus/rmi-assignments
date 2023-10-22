9deeb2f0474ba7ee19a46c49349a64132e1d863e
Diogo Jesus <diogopjesus@ua.pt>
2023-10-22 21:37:28 +0100

    Add croblink.py and update zip script.

--- zip.sh
+++ zip.sh
@@ -2,7 +2,7 @@
 
 if command -v zip >/dev/null 2>&1 ; then
     mkdir -p .tmp/agent
-    cp build.sh run.sh mainC1.py mainC2.py mainC3.py requirements.txt .tmp/agent
+    cp build.sh run.sh mainC1.py mainC2.py mainC3.py croblink.py requirements.txt .tmp/agent
     cd .tmp
     zip -r agent_92338_97596.zip agent
     mv agent_92338_97596.zip ../
